69d4492270d75b0c4d0b43fb768d857fe01b7154
Gary Hermosilla Rodríguez <garyhermosilla@gmail.com>
2024-04-18 16:03:20 -0400

    feat: added tweet icons and logic

--- app/src/main/java/com/example/jetpackcomposetweetexample/TweetScreen.kt
+++ app/src/main/java/com/example/jetpackcomposetweetexample/TweetScreen.kt
@@ -2,6 +2,7 @@ package com.example.jetpackcomposetweetexample
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +14,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -21,16 +27,25 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetweetexample.ui.theme.JetpackComposeTweetExampleTheme
 
 @Composable
 fun TweetScreen() {
+    var chat by remember {
+        mutableStateOf(false)
+    }
+    var rt by remember {
+        mutableStateOf(false)
+    }
+    var like by remember {
+        mutableStateOf(false)
+    }
     Row(
         Modifier
             .fillMaxWidth()
             .background(Color(0xFF161026))
             .padding(16.dp)
-
     ) {
         Avatar(modifier = Modifier)
         Column(
@@ -54,8 +69,105 @@ fun TweetScreen() {
                         + "when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
                 Modifier.padding(bottom = 16.dp)
             )
+
             PostImage()
+
+            Row(Modifier.padding(top = 16.dp)) {
+                SocialIcons(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_chat),
+                            contentDescription = "chat icon",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_chat_filled),
+                            contentDescription = "chat icon filled",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    isSelected = chat
+                ) {
+                    chat = !chat
+                }
+
+                SocialIcons(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "rt icon",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "rt icon",
+                            tint = Color(0xFF00FF27)
+                        )
+                    },
+                    isSelected = rt
+                ) {
+                    rt = !rt
+                }
+
+                SocialIcons(
+                    modifier = Modifier.weight(1f),
+                    unselectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_like),
+                            contentDescription = "like icon",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_like_filled),
+                            contentDescription = "like icon filled",
+                            tint = Color(0xFFFF0000)
+                        )
+                    },
+                    isSelected = like
+                ) {
+                    like = !like
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun SocialIcons(
+    modifier: Modifier,
+    unselectedIcon: @Composable () -> Unit,
+    selectedIcon: @Composable () -> Unit,
+    isSelected: Boolean,
+    onItemSelected: () -> Unit
+) {
+
+    val defaultValue = 1
+
+    Row(
+        modifier = modifier.clickable { onItemSelected() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        if (isSelected) {
+            selectedIcon()
+        } else {
+            unselectedIcon()
+        }
+
+        Text(
+            text = if (isSelected) (defaultValue + 1).toString() else defaultValue.toString(),
+            color = Color(0xFF7E8B98),
+            fontSize = 12.sp,
+            modifier = Modifier.padding(start = 4.dp)
+        )
     }
 }
 
